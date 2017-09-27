cdbaebc5dda37329365f13a73eee1daca343060e
dujinyang <309933706@qq.com>
2017-09-27 16:13:41 +0800

    author:KARL-dujinyang - ADD

---
+++ assets/js/baidu_tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3cffd45f1ea18027a194049f72f4abbf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
